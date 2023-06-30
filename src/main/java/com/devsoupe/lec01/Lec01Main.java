7ee934a33168dabc1bd08bbd4d82ed9bd8c1f822
devsoupe <luvyum4e@gmail.com>
2023-06-30 16:41:03 +0900

    1강. 코틀린에서 변수를 다루는 방법

--- src/main/java/com/devsoupe/lec01/Lec01Main.java
+++ src/main/java/com/devsoupe/lec01/Lec01Main.java
@@ -1,5 +1,6 @@
 package com.devsoupe.lec01;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
--- src/main/kotlin/com/devsoupe/lec01/Lec01Main.kt
+++ src/main/kotlin/com/devsoupe/lec01/Lec01Main.kt
@@ -6,7 +6,12 @@ fun main() {
   practice_1()
   practice_2()
   practice_3()
-  practice_4()
+//  practice_4()
+  practice_5()
+  practice_6()
+  practice_7()
+  practice_8()
+  practice_9()
 }
 
 fun practice_1() {
@@ -33,22 +38,34 @@ fun practice_3() {
 }
 
 fun practice_4() {
-  val numbers = Arrays.asList(1, 2)
+  val numbers = Arrays.asList(2, 3)
 //  numbers = mutableListOf(2, 3)
   numbers.add(3)
 }
 
 fun practice_5() {
+  val numbers = listOf(2, 3)
+//  numbers = mutableListOf(2, 3)
+//  numbers.add(3)
+}
+
+fun practice_6() {
+  val numbers = mutableListOf(2, 3)
+//  numbers = mutableListOf(2, 3)
+  numbers.add(3)
+}
+
+fun practice_7() {
   var number1: Long = 10L
   var number3: Long = 1_000L
 }
 
-fun practice_6() {
+fun practice_8() {
   var number1 = 10L
   var number3: Long? = 1_000L
   number3 = null
 }
 
-fun practice_7() {
+fun practice_9() {
   val person = Person("최태현")
 }
