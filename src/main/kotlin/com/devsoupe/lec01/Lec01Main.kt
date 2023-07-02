2f97eaad0061da85b5c18055a2aa5df5cd514843
devsoupe <luvyum4e@gmail.com>
2023-07-03 00:36:53 +0900

    2강. 코틀린에서 null을 다루는 방법

--- src/main/kotlin/com/devsoupe/lec01/Lec01Main.kt
+++ src/main/kotlin/com/devsoupe/lec01/Lec01Main.kt
@@ -3,69 +3,69 @@ package com.devsoupe.lec01
 import java.util.*
 
 fun main() {
-  practice_1()
-  practice_2()
-  practice_3()
+    practice_1()
+    practice_2()
+    practice_3()
 //  practice_4()
-  practice_5()
-  practice_6()
-  practice_7()
-  practice_8()
-  practice_9()
+    practice_5()
+    practice_6()
+    practice_7()
+    practice_8()
+    practice_9()
 }
 
 fun practice_1() {
-  var number1 = 10L
-  number1 = 5L
+    var number1 = 10L
+    number1 = 5L
 
-  val number2 = 10L
+    val number2 = 10L
 //  number2 = 5L
 }
 
 fun practice_2() {
-  var number1: Long = 10L
-  val number2: Long = 10L
+    var number1: Long = 10L
+    val number2: Long = 10L
 }
 
 fun practice_3() {
-  var number1: Long
-  number1 = 5L
-  println(number1)
+    var number1: Long
+    number1 = 5L
+    println(number1)
 
-  val number2: Long
-  number2 = 10L
-  println(number2)
+    val number2: Long
+    number2 = 10L
+    println(number2)
 }
 
 fun practice_4() {
-  val numbers = Arrays.asList(2, 3)
+    val numbers = Arrays.asList(2, 3)
 //  numbers = mutableListOf(2, 3)
-  numbers.add(3)
+    numbers.add(3)
 }
 
 fun practice_5() {
-  val numbers = listOf(2, 3)
+    val numbers = listOf(2, 3)
 //  numbers = mutableListOf(2, 3)
 //  numbers.add(3)
 }
 
 fun practice_6() {
-  val numbers = mutableListOf(2, 3)
+    val numbers = mutableListOf(2, 3)
 //  numbers = mutableListOf(2, 3)
-  numbers.add(3)
+    numbers.add(3)
 }
 
 fun practice_7() {
-  var number1: Long = 10L
-  var number3: Long = 1_000L
+    var number1: Long = 10L
+    var number3: Long = 1_000L
 }
 
 fun practice_8() {
-  var number1 = 10L
-  var number3: Long? = 1_000L
-  number3 = null
+    var number1 = 10L
+    var number3: Long? = 1_000L
+    number3 = null
 }
 
 fun practice_9() {
-  val person = Person("최태현")
+    val person = Person("최태현")
 }
---
+++ src/main/kotlin/com/devsoupe/lec02/Lec02Main.kt
@@ -0,0 +1,64 @@
+package com.devsoupe.lec02
+
+fun main() {
+    practice_1()
+    practice_2()
+}
+
+private fun practice_1() {
+    val str: String? = "ABC"
+    println(str?.length)
+}
+
+private fun practice_2() {
+    val str: String? = null
+    println(str?.length ?: 0)
+}
+
+private fun practice_3() {
+    val person = Person("공부하는 개발자")
+    startWithA(person.name)
+}
+
+fun startWithA1(str: String?): Boolean {
+    if (str == null) {
+        throw IllegalArgumentException("null이 들어왔습니다.")
+    }
+    return str.startsWith("A")
+}
+
+fun startWithA1ByElvis(str: String?): Boolean {
+    return str?.startsWith("A") ?:
+        throw IllegalArgumentException("null이 들어왔습니다.")
+}
+
+
+fun startWithA2(str: String?): Boolean? {
+    if (str == null) {
+        return null
+    }
+    return str.startsWith("A")
+}
+
+fun startWithA2ByElvis(str: String?): Boolean? {
+    return str?.startsWith("A")
+}
+
+fun startWithA3(str: String?): Boolean {
+    if (str == null) {
+        return false
+    }
+    return str.startsWith("A")
+}
+
+fun startWithA3ByElvis(str: String?): Boolean {
+    return str?.startsWith("A") ?: false
+}
+
+fun startWith(str: String?): Boolean {
+    return str!!.startsWith("A")
+}
+
+fun startWithA(str: String): Boolean {
+    return str.startsWith("A")
+}
